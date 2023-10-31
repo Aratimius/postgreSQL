1f614651fca555485a4ce3d38d4f2dce11482c6e
Aratimius <aratimius@gmail.com>
2023-10-31 06:56:21 +0300

    homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
--- SQL-команды для создания таблиц
+-- SQL-команды для создания таблиц:
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100)
+);
+
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) NOT NULL,
+	employee_id smallint NOT NULL,
+	order_date date,
+	ship_city varchar(100)
+);
+
